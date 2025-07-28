6595c4356b7d05b6fb8c436c88082fc6a5b8204c
f4lcon <willytheweird72@gmail.com>
2025-07-28 14:08:21 +0700

    added light and dark toggle

--- cabin/src/app/page.tsx
+++ cabin/src/app/page.tsx
@@ -1,27 +1,33 @@
-import Image from "next/image";
-import Link from "next/link";
-import ImageButton from "./components/icon-button";
+// import DraggableBox from "@/components/ui/draggableUi";
+import { ButtonWithIcon } from "@/components/ui/iconButton";
+import Script from "next/script";
+import Draggable from "react-draggable";
 
 export default function Home() {
   return (
-    <div className="relative flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="relative flex flex-col items-center h-screen w-screen">
+      <Script src="./script.js" />
 
-      <div className="absolute bottom-0 left-0 w-full h-1/2 bg-accent-light -z-10"></div>
-
-      <main className="flex flex-col items-center justify-center w-full h-full flex-grow">
-        <div className="absolute w-1/2 h-1/2 bg-accent-dark border-2 border-white rounded-lg overflow-hidden">
-          <div className="absolute bg-background w-full h-9/10 bottom-0 left-0">
-          <Image src={"/icons/icon_about.jpg"} className=" " width={50} height={50} alt="bub"></Image>
-          {/* <ImageButton src="../icons/icon_about_dark.jpg" alt="about icon" href="src/lol"></ImageButton> */}
+      <div className="flex items-center justify-center h-screen w-screen">
+        <main className="flex items-center justify-center w-4/5 h-13/12 ">
+          <div className="flex w-1/2 h-1/2 bg-accent-dark border-2 border-white items-end rounded-lg overflow-hidden">
+            <div className="flex justify-self-start bg-background w-full h-9/10 bottom-0 left-0">
+              <ButtonWithIcon id="openAbout" src={"/icons/icon_about.jpg"} text={"about"} alt="bub"></ButtonWithIcon>
+            </div>
           </div>
-        </div>
-      </main>
+        </main>
+      </div>
+{/* 
+      <Draggable>
+            <p>Drag me around!</p>
+            <p className="text-sm mt-2">(Client Component)</p>
+          </Draggable> */}
 
-      {/* FOOTER: At the bottom, within the main flex column of the page */}
-      <footer className="flex gap-[24px] flex-wrap items-center justify-center mt-auto">
-        {/* Add your footer content here */}
+
+      <div className="absolute bottom-0 left-0 w-full h-1/2 bg-accent-light -z-10">
+            <footer className="absolute items-center justify-center">
         <p className="text-text-normal">Your Footer Content Here</p>
-      </footer>
+      </footer></div>
     </div>
   );
 }
