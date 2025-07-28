62a2b883d18d80b44d4ff8c456c0428848c0568d
f4lcon <willytheweird72@gmail.com>
2025-07-28 18:47:05 +0700

    about tab is now styled

--- cabin/src/app/components/main-button-array.tsx
+++ cabin/src/app/components/main-button-array.tsx
@@ -1,11 +1,13 @@
 "use client"
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { ButtonWithIcon } from "./icon-button";
+import Draggable from "react-draggable";
 
 
 export function MainButtonArray(){
   const [isAboutTabOpen, setIsAboutTabOpen] = useState(false);
+  const myRef = useRef(null);
 
   const handleOpenAbout = () => {
     setIsAboutTabOpen(!isAboutTabOpen);
@@ -17,8 +19,22 @@ export function MainButtonArray(){
 
     return (
         <>
-        <div className={`absolute bg-accent-light top-0 pointer-events-none left-0 h-1/2 w-1/2 ${isAboutTabOpen ? 'opacity-100' : 'opacity-0'}`} id="aboutTab">
-        </div>
+        {/* <Draggable nodeRef={myRef} >
+            <div ref={myRef} className={`absolute bg-accent-light top-0 pointer-events-none left-0 h-1/2 w-1/2 ${isAboutTabOpen ? 'opacity-100' : 'opacity-0'}`} id="aboutTab">
+                <div className="handle">Drag from here</div>
+            </div>
+        </Draggable> */}
+        <Draggable handle="strong" nodeRef={myRef}>
+          <div ref={myRef} className="box no-cursor" style={{display: 'flex', flexDirection: 'column'}}>
+            <strong className="cursor"><div>Drag here</div></strong>
+            <div style={{overflow: 'scroll'}}>
+              <div style={{background: 'yellow', whiteSpace: 'pre-wrap'}}>
+                I have long scrollable content with a handle
+                {'\n' + Array(40).fill('x').join('\n')}
+              </div>
+            </div>
+          </div>
+        </Draggable>
 
         <div className="flex w-full h-full items-center">
             <ButtonWithIcon onClick={handleOpenAbout} id="aboutIcon" iconLightSrc={"/icons/about_grey.png"} iconDarkSrc="/icons/about_white.png" text={"about"} alt="bub"></ButtonWithIcon>
